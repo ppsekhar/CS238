818f9fbc1dc00e0c8c54db3351bd695260a8ca17
Santosh Mohan <thatindiandude78@gmail.com>
2019-11-27 19:13:10 -0800

    Update project.py

--- project.py
+++ project.py
@@ -2,12 +2,19 @@ import sys
 import numpy as np
 import copy
 
+import argparse
+
+import pdb
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--outfilename', metavar='N', type=str,help='where to write policy')
+args = parser.parse_args()
 
 # 1 unit of $ = 1 unit of E
 MIN_UNIT = -10
 MAX_UNIT = 10
 
-POSSIBLE_ACTIONS: [0, 1, 2, 3]
+POSSIBLE_ACTIONS = [0, 1, 2, 3]
 
 TRADE_ORDER = {
     0: 1,
@@ -15,6 +22,14 @@ TRADE_ORDER = {
     3: 2,
     2: 0
 }
+
+# TODO: Enable trading with more than one partner? 
+# TRADE_ORDER = {
+#     0: [1,2],
+#     1: [3,0],
+#     3: [2,1],
+#     2: [0,3]
+# }
 
 DISCOUNT_FACTOR = 0.5
 
@@ -24,6 +39,10 @@ GRID_LOCATION = {
     2: (1, 0),
     3: (1, 1)
 }
+
+CONSUMER_CONSUMPTION = 1
+PRODUCER_PRODUCTION = 1
+
 
 # B = 0 in deficit
 # otherwise A and B both 1
@@ -48,26 +67,34 @@ def calculate_next_state(curr_state, action):
 
     # update buyer
     buyer_loc = GRID_LOCATION[buyer]
-    microgrid1 = curr_state[buyer_loc[0], buyer_loc[1]]
-    # TODO: update energy and moey based on num consumers and producers
-    microgrid1_next = (microgrid1[0], microgrid1[1], microgrid1[2] + amount, microgrid1[3] - amount)
-    next_state[buyer_loc[0], buyer_loc[1]] = microgrid1_next
+    microgrid1 = curr_state[buyer_loc[0]][buyer_loc[1]]
+
+    # Assume consumers consume 1 energy, producers produce 1
+    new_energy1 = microgrid1[2] + amount - microgrid1[1]*CONSUMER_CONSUMPTION \
+                    + microgrid1[0]*PRODUCER_PRODUCTION
+    microgrid1_next = (microgrid1[0], microgrid1[1], new_energy1, microgrid1[3] - amount)
+    next_state[buyer_loc[0]][buyer_loc[1]] = microgrid1_next
 
     # update seller
-    seller_loc = GRID_LOCATION[buyer]
-    microgrid2 = curr_state[seller_loc[0], seller_loc[1]]
-    # TODO: update energy and moey based on num consumers and producers
-    microgrid2_next = (microgrid2[0], microgrid2[1], microgrid2[2] - amount, microgrid2[3] + amount)
-    next_state[seller_loc[0], seller_loc[1]] = microgrid2_next
+    seller_loc = GRID_LOCATION[seller]
+    microgrid2 = curr_state[seller_loc[0]][seller_loc[1]]
+
+    new_energy2 =  microgrid2[2] - amount - microgrid2[1]*CONSUMER_CONSUMPTION \
+                    + microgrid2[0]*PRODUCER_PRODUCTION
+    microgrid2_next = (microgrid2[0], microgrid2[1], new_energy2, microgrid2[3] + amount)
+    next_state[seller_loc[0]][seller_loc[1]] = microgrid2_next
 
     return next_state
 
 
+# def generate_states(min_bound, max_bound): 
 
 
-def value_iteration(state):
+def value_iteration(initial_state):
+
     # state: (action, utility)
     # action: (buyer, seller, $ amount)
+    state = copy.deepcopy(initial_state)
     u_vals = {}
     for i in range(3): # TODO: end at convergence
         # TODO: loop through all states
@@ -82,26 +109,36 @@ def value_iteration(state):
             (ex. same total money as initialized, etc.). This makes the microgrid
             initializations matter, but is probably more difficult to do.
         '''
-        if state not in u_vals:
+        state_key = flatten_microgrid(state)
+        if state_key not in u_vals:
             # default:
                 # utility: 0
                 # action: top players don't trade
-            u_vals[state] = ((0, 1, 0), 0)
+            u_vals[state_key] = ((0, 1, 0), 0)
+
         best_action = (0, 1, 0)
         best_u = 0
         first_action = True
         for trader in TRADE_ORDER:
             for action_amount in POSSIBLE_ACTIONS:
+
+                # Sell 
                 curr_action1 = (trader, TRADE_ORDER[trader], action_amount)
                 next_state1 = calculate_next_state(state, curr_action1)
-                if next_state1 not in u_vals:
-                    u_vals[next_state1] = ((0, 1, 0), 0)
-                u1 = reward(next_state1) + DISCOUNT_FACTOR*u_vals[next_state1]
+                next_state1_key = flatten_microgrid(next_state1)
+                
+                if next_state1_key not in u_vals:
+                    u_vals[next_state1_key] = ((0, 1, 0), 0)
+                u1 = reward(next_state1) + DISCOUNT_FACTOR*u_vals[next_state1_key][1]
+                
+                # Buy 
                 curr_action2 = (TRADE_ORDER[trader], trader, action_amount)
                 next_state2 = calculate_next_state(state, curr_action2)
-                if next_state2 not in u_vals:
-                    u_vals[next_state2] = ((0, 1, 0), 0)
-                u2 = reward(next_state2) + DISCOUNT_FACTOR * u_vals[next_state2]
+                next_state2_key = flatten_microgrid(next_state2)
+                
+                if next_state2_key not in u_vals:
+                    u_vals[next_state2_key] = ((0, 1, 0), 0)
+                u2 = reward(next_state2) + DISCOUNT_FACTOR * u_vals[next_state2_key][1]
 
                 if first_action or u1 > best_u:
                     best_action = curr_action1
@@ -110,16 +147,22 @@ def value_iteration(state):
                 if u2 > best_u:
                     best_action = curr_action2
                     best_u = u2
-        u_vals[state] = (best_action, best_u)
+        u_vals[state_key] = (best_action, best_u)
 
 
 
 
 
 # TODO: deal with states not visited during value iteration
-def extract_policy(u_vals):
+def extract_policy(u_vals, output_filename):
     pass
 
+# Flatten microgrid into single tuple for hashing
+def flatten_microgrid(state): 
+    hashable_tuple = ()
+    for loc in state: 
+        hashable_tuple = hashable_tuple + tuple(loc)
+    return hashable_tuple
 
 # TODO: Generalize to worlds larger than 2x2
 def main():
@@ -131,10 +174,13 @@ def main():
         [(2, 2, 10, 10), (2, 2, 10, 10)],
         [(2, 2, 10, 10), (2, 2, 10, 10)]
     ]
+    flatten_microgrid(initial_state)
+
     # TODO: retrieve outfilename from args
     # print(reward(initial_state))
     u_vals = value_iteration(initial_state)
-    extract_policy(initial_state, outfilename)
+
+    extract_policy(u_vals, args.outfilename)
 
 
 if __name__ == '__main__':
